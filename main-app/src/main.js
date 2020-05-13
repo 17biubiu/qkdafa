83f00a6e5434124b590a41ca22234b85e8ea408a
17biu <nnhk001@163.com>
2020-05-13 21:25:04 +0800

    feat: 初始化微应用

---
+++ main-app/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import startQiankun from './micro'
+Vue.use(Antd)
+
+startQiankun()
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ main-app/src/micro/app.js
@@ -0,0 +1,10 @@
+const apps = [
+  {
+    name: 'VueMicroApp',
+    entry: '//localhost:10200',
+    container: '#frame',
+    activeRule: '/vue'
+  }
+]
+
+export default apps
